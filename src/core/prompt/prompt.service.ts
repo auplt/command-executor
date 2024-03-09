c3517783a003033459765d86341dd1ba2d8445a8
auplt <98539159+auplt@users.noreply.github.com>
2024-03-09 16:29:24 +0300

    Ffmpeg executor added

--- src/core/prompt/prompt.service.ts
+++ src/core/prompt/prompt.service.ts
@@ -2,7 +2,7 @@ import inquirer from "inquirer";
 import { PromptType } from "./prompt.types.js";
 
 export class PromptService {
-  public async input<T extends PromptType>(message: string, type: PromptType) {
+  public async input<T>(message: string, type: PromptType) {
     //   name defines key in the answer where the result is stored
     const { result } = await inquirer.prompt<{ result: T }>([
       {
